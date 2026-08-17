7272dac9e60027335f74bc7627b8c452ac7ca438
Mohit Kushwaha <tech.mohit26@gmail.com>
2026-08-17 18:54:02 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/constants.php';
 
 try {
     $pdo = new PDO(
-        'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+        'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4',
         DB_USER,
         DB_PASS,
         [
